99d19a0a95be908158596dc5db1deec7dbfc1471
soufiane <soufianeelbasraoui0@gmail.com>
2026-03-30 12:43:35 +0200

    ÃÃÃinitial commit

---
+++ src/test/java/org/fleetflow/fleetflow/FleetFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fleetflow.fleetflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FleetFlowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
