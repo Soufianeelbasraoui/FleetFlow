6f84e5ee95be8435c87ebed5b9e729b3412ee00c
Youssef <yousseferrachid@gmail.com>
2026-03-31 16:26:49 +0100

    changes

--- src/main/java/org/fleetflow/fleetflow/entity/Chauffeur.java
+++ src/main/java/org/fleetflow/fleetflow/entity/Chauffeur.java
@@ -1,4 +1,24 @@
 package org.fleetflow.fleetflow.entity;
 
-public class Chauffeu {
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Chauffeur {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nom;
+    private String telephone;
+
+    @OneToMany(mappedBy = "chauffeur")
+    private List<Livraison> liveries;
 }
