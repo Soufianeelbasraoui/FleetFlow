eac8fb71956f27e02b1c17eaf6bb84364d361cf3
Youssef <yousseferrachid@gmail.com>
2026-03-31 16:12:55 +0100

    changes

--- src/main/java/org/fleetflow/fleetflow/entity/Chauffeu.java
+++ src/main/java/org/fleetflow/fleetflow/entity/Chauffeu.java
@@ -15,9 +15,11 @@ public class Chauffeu {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String nom;
     private String telephone;
+
+
+
 
     @OneToMany(mappedBy = "chauffeur")
     private List<Livraison> liveries;
