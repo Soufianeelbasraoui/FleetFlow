8e0726481632afae60d849b0ca3471e299c43277
soufiane <soufianeelbasraoui0@gmail.com>
2026-03-31 16:31:50 +0200

    add branch soufiane

--- src/main/java/org/fleetflow/fleetflow/entity/Client.java
+++ src/main/java/org/fleetflow/fleetflow/entity/Client.java
@@ -1,4 +1,19 @@
 package org.fleetflow.fleetflow.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "Client")
+@Getter
+@Setter
 public class Client {
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private Long id;
+private String nom;
+private String email;
+private String telephone;
+private String ville;
 }
