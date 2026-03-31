dd795ff9d25859c9ae13e6649fd809058a1bd65a
Youssef <yousseferrachid@gmail.com>
2026-03-31 16:00:20 +0100

    creating Entities Chauffeur, Vehicule

--- src/main/java/org/fleetflow/fleetflow/entity/Chauffeu.java
+++ src/main/java/org/fleetflow/fleetflow/entity/Chauffeu.java
@@ -1,4 +1,24 @@
 package org.fleetflow.fleetflow.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Data
+@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Chauffeu {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nom;
+    private String telephone;
+
+    @OneToMany(mappedBy = "chauffeur")
+    private List<Livraison> liveries;
 }
