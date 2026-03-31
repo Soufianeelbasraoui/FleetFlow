8ea267fb8770fda2adc8daebdcdd0d6c532d97cd
soufiane <soufianeelbasraoui0@gmail.com>
2026-03-31 17:00:38 +0200

    add entity client and Livraison

--- src/main/java/org/fleetflow/fleetflow/entity/Client.java
+++ src/main/java/org/fleetflow/fleetflow/entity/Client.java
@@ -1,13 +1,15 @@
 package org.fleetflow.fleetflow.entity;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
+
+import java.util.List;
 
 @Entity
 @Table(name = "Client")
-@Getter
-@Setter
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Client {
 @Id
 @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,4 +18,7 @@ private String nom;
 private String email;
 private String telephone;
 private String ville;
+
+@OneToMany(mappedBy = "client")
+private List<Livraison> livraisons;
 }
