6749986049c77ddce1e256bda4f96c3f8dcd397d
soufiane <soufianeelbasraoui0@gmail.com>
2026-04-02 10:33:22 +0200

    update

--- src/main/java/org/fleetflow/fleetflow/mapper/ClientMapper.java
+++ src/main/java/org/fleetflow/fleetflow/mapper/ClientMapper.java
@@ -1,4 +1,15 @@
 package org.fleetflow.fleetflow.mapper;
 
-public class ClientMapper {
+import org.fleetflow.fleetflow.dto.ClientDTO;
+import org.fleetflow.fleetflow.entity.Client;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface ClientMapper {
+    ClientDTO toDTO(Client client);
+    Client toEntity(ClientDTO clientDTO);
+    List<ClientDTO> toDTOs(List<Client> clients);
+    List<Client>  toEntities(List<ClientDTO> clientDTOS);
 }
