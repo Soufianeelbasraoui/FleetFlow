306c0d519013607261db30562cb6df20cc137886
soufiane <soufianeelbasraoui0@gmail.com>
2026-04-03 13:56:32 +0200

    add dockerfile

--- src/main/java/org/fleetflow/fleetflow/repository/LivraisonRepository.java
+++ src/main/java/org/fleetflow/fleetflow/repository/LivraisonRepository.java
@@ -16,12 +16,10 @@ public interface LivraisonRepository extends JpaRepository<Livraison, Long> {
 
     List<Livraison> findByClientId(Long clientId);
 
-    // @Query — between two dates
     @Query("SELECT l FROM Livraison l WHERE l.dateLivraison BETWEEN :debut AND :fin")
     List<Livraison> findByDateLivraisonBetween(@Param("debut") LocalDate debut,
                                                @Param("fin")   LocalDate fin);
 
-    // @Query — by destination city
     @Query("SELECT l FROM Livraison l WHERE l.adresseDestination LIKE %:ville%")
     List<Livraison> findByVilleDestination(@Param("ville") String ville);
 }
