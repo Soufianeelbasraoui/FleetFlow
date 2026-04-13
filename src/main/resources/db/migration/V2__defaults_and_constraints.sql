d0c38c485d0c5052fb1fd36891234979e3ee7e8e
soufiane <soufianeelbasraoui0@gmail.com>
2026-04-13 18:12:01 +0200

    fix errour flyway  migration

---
+++ src/main/resources/db/migration/V2__defaults_and_constraints.sql
@@ -0,0 +1,24 @@
+
+ALTER TABLE chauffeur MODIFY available BOOLEAN DEFAULT TRUE;
+ALTER TABLE vehicule  MODIFY statut_vehicule VARCHAR(255) DEFAULT 'AVAILABLE';
+ALTER TABLE livraison MODIFY statut VARCHAR(255) DEFAULT 'PENDING';
+
+
+ALTER TABLE vehicule
+    ADD CONSTRAINT chk_vehicule_capacite CHECK (capacite > 0);
+
+
+ALTER TABLE livraison
+    ADD CONSTRAINT fk_livraison_client
+        FOREIGN KEY (client_id) REFERENCES client(id)
+            ON DELETE RESTRICT ON UPDATE CASCADE;
+
+ALTER TABLE livraison
+    ADD CONSTRAINT fk_livraison_chauffeur
+        FOREIGN KEY (chauffeur_id) REFERENCES chauffeur(id)
+            ON DELETE SET NULL ON UPDATE CASCADE;
+
+ALTER TABLE livraison
+    ADD CONSTRAINT fk_livraison_vehicule
+        FOREIGN KEY (vehicule_id) REFERENCES vehicule(id)
+            ON DELETE SET NULL ON UPDATE CASCADE;
