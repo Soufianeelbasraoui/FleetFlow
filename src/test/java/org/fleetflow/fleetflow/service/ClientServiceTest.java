201bfdc4b59cb737d5052fa3b653c15695919ca3
soufiane <soufianeelbasraoui0@gmail.com>
2026-04-15 13:54:24 +0200

    add test livraison

--- src/test/java/org/fleetflow/fleetflow/service/ClientServiceTest.java
+++ src/test/java/org/fleetflow/fleetflow/service/ClientServiceTest.java
@@ -10,8 +10,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 import static org.junit.jupiter.api.Assertions.*;
 @ExtendWith(MockitoExtension.class)
