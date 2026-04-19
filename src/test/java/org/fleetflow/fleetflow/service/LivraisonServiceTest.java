255e9f40f272b3814603e5f913c99c29907f79f2
soufiane <soufianeelbasraoui0@gmail.com>
2026-04-19 23:27:25 +0200

    update test

--- src/test/java/org/fleetflow/fleetflow/service/LivraisonServiceTest.java
+++ src/test/java/org/fleetflow/fleetflow/service/LivraisonServiceTest.java
@@ -36,72 +36,77 @@ class LivraisonServiceTest {
     @InjectMocks
     LivraisonService livraisonService;
     @Test
-    void creerLivraison() {
-        LivraisonDTO livraisonDTOS=new LivraisonDTO( LocalDate.of(2026, 4, 15),"casa","oudeZem","EN_ATTENTE",1L,2L,3L);
-        Livraison livraisons=new Livraison();
-        livraisons.setId(1L);
-        livraisons.setDateLivraison(LocalDate.of(2026, 4, 15));
-        livraisons.setStatut("EN_ATTENTE");
-        Mockito.when(livraisonRepository.save(Mockito.any())).thenReturn(livraisons);
-        Mockito.when(livraisonMapper.toDTO(livraisons)).thenReturn(livraisonDTOS);
-        Mockito.when(livraisonMapper.toEntity(livraisonDTOS)).thenReturn(livraisons);
+//    void creerLivraison() {
+//        LivraisonDTO livraisonDTOS=new LivraisonDTO( LocalDate.of(2026, 4, 15),"casa","oudeZem","EN_ATTENTE",1L,2L,3L);
+//        Livraison livraisons=new Livraison();
+//        livraisons.setId(1L);
+//        livraisons.setDateLivraison(LocalDate.of(2026, 4, 15));
+//        livraisons.setStatut("EN_ATTENTE");
+//        Mockito.when(livraisonRepository.save(Mockito.any())).thenReturn(livraisons);
+//        Mockito.when(livraisonMapper.toDTO(livraisons)).thenReturn(livraisonDTOS);
+//        Mockito.when(livraisonMapper.toEntity(livraisonDTOS)).thenReturn(livraisons);
+//
+//        LivraisonDTO resultDto=livraisonService.creerLivraison(livraisonDTOS);
+//        assertEquals("EN_ATTENTE",resultDto.getStatut());
+//        Mockito.verify(livraisonRepository).save(Mockito.any());
+//    }
+    void creerLivraison(){
+        LivraisonDTO dtos=new LivraisonDTO();
+        dtos.setStatut("EN_ATTENTE");
 
-        LivraisonDTO resultDto=livraisonService.creerLivraison(livraisonDTOS);
-        assertEquals("EN_ATTENTE",resultDto.getStatut());
-        Mockito.verify(livraisonRepository).save(Mockito.any());
+        Livraison entity=new Livraison();
+        entity.setStatut("EN_ATTENTE");
+
+        Mockito.when(livraisonRepository.save(entity)).thenReturn(entity);
+        Mockito.when(livraisonMapper.toDTO(entity)).thenReturn(dtos);
+        Mockito.when(livraisonMapper.toEntity(dtos)).thenReturn(entity);
+
+        LivraisonDTO result=livraisonService.creerLivraison(dtos);
+
+        assertEquals("EN_ATTENTE",result.getStatut());
     }
 
 
     @Test
-    void assignerChauffeurEtVehicule() {
-        Long livraisonId = 1L;
-        Long chauffeurId = 2L;
-        Long vehiculeId = 3L;
-
-        Livraison livraison=new Livraison();
-        livraison.setId(livraisonId);
-        Chauffeur chauffeur=new Chauffeur();
-        chauffeur.setId(chauffeurId);
-        Vehicule vehicule=new Vehicule();
-        vehicule.setId(vehiculeId);
-
-        LivraisonDTO livraisonDTO=new LivraisonDTO();
-        livraisonDTO.setChauffeurId(chauffeurId);
-        livraisonDTO.setVehiculeId(vehiculeId);
-
-        Mockito.when(livraisonRepository.findById(livraisonId)).thenReturn(Optional.of(livraison));
-        Mockito.when(chauffeurRepository.findById(chauffeurId)).thenReturn(Optional.of(chauffeur));
-        Mockito.when(vehiculeRepository.findById(vehiculeId)).thenReturn(Optional.of(vehicule));
-        Mockito.when(livraisonRepository.save(Mockito.any())).thenReturn(livraison);
-        Mockito.when(livraisonMapper.toDTO(livraison)).thenReturn(livraisonDTO);
-        LivraisonDTO result = livraisonService.assignerChauffeurEtVehicule(livraisonId, chauffeurId, vehiculeId);
-
-        assertNotNull(result);
-        assertEquals(chauffeurId, result.getChauffeurId());
-        assertEquals(vehiculeId, result.getVehiculeId());
-        Mockito.verify(livraisonRepository).save(Mockito.any());
-    }
-
-    @Test
-    void changerStatut() {
-
-        Long livraisonId = 1L;
+    void testAssignation() {
 
         Livraison livraison = new Livraison();
-        livraison.setId(livraisonId);
+        Chauffeur chauffeur = new Chauffeur();
+        Vehicule vehicule = new Vehicule();
+
+        Mockito.when(livraisonRepository.findById(1L)).thenReturn(Optional.of(livraison));
+        Mockito.when(chauffeurRepository.findById(2L)).thenReturn(Optional.of(chauffeur));
+        Mockito.when(vehiculeRepository.findById(3L)).thenReturn(Optional.of(vehicule));
+        Mockito.when(livraisonRepository.save(livraison)).thenReturn(livraison);
+
+        LivraisonDTO dto = new LivraisonDTO();
+        dto.setChauffeurId(2L);
+        dto.setVehiculeId(3L);
+
+        Mockito.when(livraisonMapper.toDTO(livraison)).thenReturn(dto);
+
+        LivraisonDTO result = livraisonService.assignerChauffeurEtVehicule(1L, 2L, 3L);
+
+        assertEquals(2L, result.getChauffeurId());
+        assertEquals(3L, result.getVehiculeId());
+    }
+
+    @Test
+    void testChangerStatut() {
+
+        Livraison livraison = new Livraison();
         livraison.setStatut("EN_ATTENTE");
 
-        LivraisonDTO livraisonDTO = new LivraisonDTO();
-        livraisonDTO.setStatut("LIVREE");
+        Mockito.when(livraisonRepository.findById(1L)).thenReturn(Optional.of(livraison));
+        Mockito.when(livraisonRepository.save(livraison)).thenReturn(livraison);
 
-        Mockito.when(livraisonRepository.findById(livraisonId)).thenReturn(Optional.of(livraison));
-        Mockito.when(livraisonRepository.save(Mockito.any())).thenReturn(livraison);
+        LivraisonDTO dto = new LivraisonDTO();
+        dto.setStatut("LIVREE");
 
-        Mockito.when(livraisonMapper.toDTO(livraison)).thenReturn(livraisonDTO);
-        LivraisonDTO result = livraisonService.changerStatut(livraisonId, "LIVREE");
+        Mockito.when(livraisonMapper.toDTO(livraison)).thenReturn(dto);
+
+        LivraisonDTO result = livraisonService.changerStatut(1L, "LIVREE");
 
         assertEquals("LIVREE", result.getStatut());
-
-        Mockito.verify(livraisonRepository).save(Mockito.any());
     }
 }
